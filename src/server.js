c95eade45790d86dda64152dcc54893cb6f3e178
Steffan Sluis <steffansluis@gmail.com>
2017-05-17 22:57:49 +0200

    Fix default PG url

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ const morgan = require('morgan');
 
 const { SERVER_PORT } = require('./config');
 
+const PG = require('./pg');
+
 const server = express();
 
 server.use(morgan('combined'));
@@ -10,8 +12,18 @@ server.use(morgan('combined'));
 // Disable favicon
 server.get('/favicon.ico', (req, res, next) => res.status(404).end());
 
-server.get('/', (req, res) => {
-  res.json({ msg: 'hello world!' });
+server.get('/:type', (req, res, next) => {
+  const { type } = req.params;
+  PG.findAll(type)
+    .then(results => {
+      console.log(`Results for ${type}:`, results);
+      res.json(results);
+    }).catch(next);
+});
+
+server.use((error, req, res) => {
+  console.error(error);
+  res.status(400).json({ error: error });
 });
 
 console.log(`Starting server on port ${SERVER_PORT}`);
