03d33631eb183e7549b5c56453bd77b3937bce33
joostrothweiler <joostrothweiler@gmail.com>
2017-05-24 15:13:03 +0200

    Add functionality for actors and start of genres. Also allow for same type of model selection as for PG

--- src/neo/index.js
+++ src/neo/index.js
@@ -1,11 +1,22 @@
-const Movies = require('./models/movies');
 const connection = require('./connection');
+const Models = require('./models');
 
+const typeMap = {
+  movies: 'Movie',
+  actors: 'Actor'
+}
+
+function _getModel(type) {
+  if (!(type in typeMap)) throw new Error(`'${type}' is not a valid model name.`);
+  let key = typeMap[type];
+  return Models[key];
+}
 
 async function findAll(req, type, page = 0, perPage = 10) {
   console.log(`Finding ${type} page ${page} per ${perPage}`);
 
-  return Movies.findAll(connection.getSession(req));
+  const Model = _getModel(type);
+  return Model.findAll(connection.getSession(req));
 }
 
 module.exports = {
--- src/neo/models/neo4j/genre.js
+++ src/neo/models/neo4j/genre.js
@@ -1,10 +1,10 @@
 const _ = require('lodash');
 
-const Movie = function (_node) {
+const Genre = (_node) => {
   _.extend(this, _node.properties);
   if (this.id) {
-    this.id = this.id.toNumber();
+  	this.id = this.id.toNumber();
   }
 };
 
-module.exports = Movie;
+module.exports = Genre;
