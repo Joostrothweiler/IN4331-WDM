2bb54c3fa39ada5085840bde267667a4ea756787
Roos de Kok <r.e.dekok@student.tudelft.nl>
2017-06-12 14:52:17 +0200

    WIP: working on scenario 1 and 2 (movies and actors info)

--- src/mongo/connection.js
+++ src/mongo/connection.js
@@ -1,18 +1,6 @@
 // https://www.packtpub.com/books/content/mongo-goes-to-the-movies
+// https://mongodb.github.io/node-mongodb-native/markdown-docs/queries.html
 // http://mongodb.github.io/node-mongodb-native/2.2/tutorials/projections/
-
-var findMovies = function(db, title, kind, year, callback) {
-    // Get the movies collection
-    var movies = db.collection('movies');
-
-    // Find movie on title ($regex allows partial search)
-    movies.find({"title": {"$regex": title}, "kind": kind, "year": year}, {"_id": 0, "kind": 0}).toArray(function(err, movs) {
-        assert.equal(err, null);
-        console.log("Found the following movies:");
-        console.log(movs);
-        callback(movs);
-    });
-};
 
 var MongoClient = require('mongodb').MongoClient
     , assert = require('assert');
@@ -25,27 +13,17 @@ MongoClient.connect(url, function(err, db) {
     assert.equal(null, err);
     console.log("Connected successfully to server");
 
-    var title = "Star Wars";
-    var kind = "movie";
-    var year = [ 2016 ];
-
-    // Allows for partial search
-    var regexSearchOptions = {
-        "title": {
-            "$regex": title
-        }
-    };
-
-    findMovies(db, title, kind, year, function () {
-        db.close();
-    })
-
-    // insertDocuments(db, function() {
-    //     indexCollection(db, function() {
-    //         db.close();
-    //     });
-    // });
+    db.close();
 });
+
+// Mixing AND and OR statements:
+// {
+//     title:"MongoDB",
+//         $or:[
+//     {author:"Daniel"},
+//     {author:"Jessica"}
+// ]
+// }
 
 // Import data using the command line:
 // >> cd "C:\Program Files\MongoDB\Server\3.4\bin"
