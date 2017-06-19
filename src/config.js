f8878216b043e62eedd3c805ed6a78438021751f
Arjo <arjovanramshorst@gmail.com>
2017-06-19 11:34:12 +0200

    Fix connection strings for mongo and postgres.

--- src/config.js
+++ src/config.js
@@ -3,10 +3,10 @@ require('dotenv').load({ silent: true });
 const {
   SERVER_PORT = 3000,
   POSTGRES_DBNAME = 'wdm',
-  POSTGRES_URL = 'postgres://postgres:postgres@postgres:5432/',
+  POSTGRES_URL = 'postgres://postgres:postgres@pg-master:5432/',
   NEO_URL = 'bolt://neo4j',
   BASE_URL = 'http://localhost:2222',
-  MONGO_URL = 'mongo'
+  MONGO_URL = 'mongo1,mongo2,mongo3'
 } = process.env;
 
 module.exports = {
