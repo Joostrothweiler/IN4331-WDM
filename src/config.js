af6de4bffddd78529a251d418c6839be37a3763e
Arjo <arjovanramshorst@gmail.com>
2017-06-19 11:47:27 +0200

    Add sequelize config strings for replication.

--- src/config.js
+++ src/config.js
@@ -3,7 +3,8 @@ require('dotenv').load({ silent: true });
 const {
   SERVER_PORT = 3000,
   POSTGRES_DBNAME = 'wdm',
-  POSTGRES_URL = 'postgres://postgres:postgres@pg-master:5432/',
+  POSTGRES_MASTER_URL = 'postgres://postgres:postgres@pg-master:5432/',
+  POSTGRES_SLAVES_URL = ['postgres://postgres:postgres@pg-slave1:5432/', 'postgres://postgres:postgres@pg-slave2:5432/'],
   NEO_URL = 'bolt://neo4j',
   BASE_URL = 'http://localhost:2222',
   MONGO_URL = 'mongo1,mongo2,mongo3'
@@ -11,7 +12,8 @@ const {
 
 module.exports = {
     SERVER_PORT,
-    POSTGRES_URL,
+    POSTGRES_MASTER_URL,
+    POSTGRES_SLAVES_URL,
     POSTGRES_DBNAME,
     NEO_URL,
     BASE_URL,
