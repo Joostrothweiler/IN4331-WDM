23f51d0c50a72968be8d6a222e23b3808e369797
Arjo <arjovanramshorst@gmail.com>
2017-06-19 11:11:34 +0200

    Add mongodb replication, initialization for mongdb replication, and add postgresql replication.

--- src/mongo/initialize-rs.js
+++ src/mongo/initialize-rs.js
@@ -1,10 +1,42 @@
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
 
-const MongoClient = require('mongodb').MongoClient
-const Server = require('mongodb').Server;
+const url = "mongodb://mongo1/"
+MongoClient.connect(url)
+    .then((db) => {
+        console.log('successfully connected')
+        const initReplSet = () => {
+            db.command({
+                replSetInitiate: {
+                    _id: 'mongo-replication',
+                    members: [
+                        { _id: 0, host: 'mongo1'},
+                        { _id: 1, host: 'mongo2'},
+                        { _id: 2, host: 'mongo3'},
+                    ]
+                }
+            }).then(res => {
+                console.log('Successfully initialized the replication set.')
+                db.close()
 
-const client = new MongoClient(new Server('mongo'))
+            }).catch(err => {
+                console.log('Something went wrong:')
+                console.log(err)
+                db.close()
+            })
+        }
+        db.command({'replSetGetStatus': 1})
+            .then(res => {
+                console.log('ReplicaSet already initiated')
+                console.log('Closing connection now...')
+                db.close()
+            }).catch(err => {
+                console.log('ReplicaSet not yet initiated, doing it now')
+                initReplSet()
 
-client.open((err, client) => {
-    console.log(client.rs.status());
+        })
+
+    }).catch((err) => {
+        console.log('error?')
 })
 
