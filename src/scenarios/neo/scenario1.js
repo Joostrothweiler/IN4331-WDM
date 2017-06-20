6d6ff4755a890a1bebcdb697c244438a4f32ae1f
joostrothweiler <joostrothweiler@gmail.com>
2017-06-20 20:22:36 +0200

    scenario 3 neo

--- src/scenarios/neo/scenario1.js
+++ src/scenarios/neo/scenario1.js
@@ -4,25 +4,7 @@ module.exports = (req, res, next) => {
   const { id } = req.params;
   const { page = 0, perPage = 10, dir = 'asc', orderby = 'year',  title, from, to } = req.query;
 
-  let where = Object.assign({}, req.query, {
-    title: {
-      $like: `%${title}`,
-    },
-    year: {
-      $gte: from || 1,
-      $lte: to != null ? to : (new Date()).getFullYear()
-    },
-    type: 3
-  });
-
-  if (title == null) delete where.title;
-  if (title == null) delete where.type;
-  delete where.page;
-  delete where.perPage;
-  delete where.dir;
-
-  delete where.from;
-  delete where.to;
+  let where = Object.assign({}, req.query)
 
   if (id == null) return ORM.findAll('neo', 'movies', { where, page, perPage, orderby, dir })
       .then(results => {
--- src/scenarios/neo/scenario2.js
+++ src/scenarios/neo/scenario2.js
@@ -5,20 +5,8 @@ module.exports = (req, res, next) => {
   const { id } = req.params;
   const { page = 0, perPage = 10, dir = 'asc', orderby = 'year', title, from, to } = req.query;
 
-  let where = Object.assign({}, req.query, {
-  });
+  let where = {}
 
-  if (title == null) delete where.title;
-  if (title == null) delete where.type;
-  delete where.page;
-  delete where.perPage;
-  delete where.dir;
-  delete where.order;
-
-  delete where.from;
-  delete where.to;
-
-  // let order = `asc ${orderby}, n.year`;
   let order = [
     [ 'actor.id', 'asc'],
     [ `n.${orderby}`, dir]
@@ -28,8 +16,6 @@ module.exports = (req, res, next) => {
       .then(results => {
         res.json(results);
       }).catch(next);
-
-  where.id = id;
 
   return ORM.find('neo', 'actors', { id } )
     .then(result => {
